d3316cf03447b620b59b51feee9399a2f5a9629d
wander <wksw123456@gmail.com>
2024-07-06 18:11:16 +0800

    更新命名

--- asjard.go
+++ asjard.go
@@ -55,7 +55,7 @@ type Asjard struct {
 	// 已启动的服务
 	startedServers []string
 	// 退出信号
-	done chan struct{}
+	exit chan struct{}
 }
 
 // New 入口
@@ -63,7 +63,7 @@ func New() *Asjard {
 	return &Asjard{
 		handlers: make(map[string][]any),
 		startErr: make(chan error),
-		done:     make(chan struct{}),
+		exit:     make(chan struct{}),
 	}
 }
 
@@ -176,15 +176,15 @@ func (asd *Asjard) Start() error {
 		logger.Error("start error:",
 			"error", err)
 	}
-	close(asd.done)
+	close(asd.exit)
 	// 系统停止
 	asd.stop()
 	return nil
 }
 
 // Exit 退出信号
-func (asd *Asjard) Done() <-chan struct{} {
-	return asd.done
+func (asd *Asjard) Exit() <-chan struct{} {
+	return asd.exit
 }
 
 // 启动所有服务
