0a4677160c9a42bf0eb7d42d4936c302539a352f
wander <wksw123456@gmail.com>
2024-07-18 20:16:19 +0800

    update gw example

--- examples/gw/main.go
+++ examples/gw/main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/asjard/asjard"
+	"github.com/asjard/asjard/core/logger"
 	pb "github.com/asjard/asjard/examples/protobuf/hello"
 	"github.com/asjard/asjard/pkg/server/rest"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 var _ pb.HelloServer = &pb.HelloAPI{}
 var _ rest.Handler = &pb.HelloAPI{}
 
+type Rewrite struct {
+	*pb.HelloAPI
+	exit <-chan struct{}
+}
+
+func (api Rewrite) Log(ctx context.Context, in *emptypb.Empty) (*emptypb.Empty, error) {
+	logger.Debug("------is stream request-------")
+	rtx, ok := ctx.(*rest.Context)
+	if !ok {
+		return nil, status.Error(codes.Unimplemented, "unsupport protocol")
+	}
+	rtx.SetContentType("text/event-stream")
+	rtx.SetBodyStreamWriter(func(w *bufio.Writer) {
+		for {
+			select {
+			case <-api.exit:
+				logger.Debug("------server done----------")
+				return
+			default:
+				w.Write([]byte(fmt.Sprintf("data: %s\n\n", time.Now())))
+
+				if err := w.Flush(); err != nil {
+					logger.Debug("client disconnected", "err", err)
+					return
+				}
+
+				time.Sleep(time.Second)
+			}
+		}
+	})
+	return nil, nil
+}
+
 func main() {
 	server := asjard.New()
-	server.AddHandler(&pb.HelloAPI{}, rest.Protocol)
+	server.AddHandler(&Rewrite{
+		HelloAPI: &pb.HelloAPI{},
+		exit:     server.Exit(),
+	}, rest.Protocol)
 	if err := server.Start(); err != nil {
 		panic(err)
 	}
