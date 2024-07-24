3e1eff7e03a481ba5d47f609c58b74bc4b9fa17d
wander <wksw123456@gmail.com>
2024-07-24 16:44:31 +0800

    update rest readEntity

--- pkg/server/rest/context.go
+++ pkg/server/rest/context.go
@@ -150,7 +150,7 @@ func (c *Context) Close() {
 
 // ReadBodyParams 读取请求体
 func (c *Context) ReadBodyParams() []byte {
-	if !c.postLoaded && len(c.Request.Header.MultipartFormBoundary()) == 0 {
+	if !c.postLoaded && string(c.Request.Header.ContentType()) == "application/json" {
 		c.postBody = c.Request.Body()
 		c.postLoaded = true
 	}
