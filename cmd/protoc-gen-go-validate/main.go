aed7f224c325c3cf61398de1dc155233b1cb17ea
wander <wksw123456@gmail.com>
2024-09-10 09:51:11 +0800

    feature validate

---
+++ cmd/protoc-gen-go-validate/main.go
@@ -0,0 +1,89 @@
+/*
+ *
+ * Copyright 2024 ASJARD authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// protoc-gen-go-validate is a plugin for the Google protocol buffer compiler to
+// generate Go code. Install it by building this program and making it
+// accessible within your PATH with the name:
+//
+//	protoc-gen-go-validate
+//
+// The 'go-validate' suffix becomes part of the argument for the protocol compiler,
+// such that it can be invoked as:
+//
+//	protoc --go-validate_out=. path/to/file.proto
+//
+// This generates Go service definitions for the protocol buffer defined by
+// file.proto.  With that input, the output will be written to:
+//
+//	path/to/file_validate.pb.go
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+const (
+	version = "1.0.0"
+	name    = "protoc-gen-go-validate"
+)
+
+var requireUnimplemented *bool
+var useGenericStreams *bool
+var flags flag.FlagSet
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("%s %v\n", name, version)
+		return
+	}
+
+	// conf := openapi.Configuration{
+	// 	Version:         flags.String("version", "0.0.1", "version number text, e.g. 1.2.3"),
+	// 	Title:           flags.String("title", "", "name of the API"),
+	// 	Description:     flags.String("description", "", "description of the API"),
+	// 	Naming:          flags.String("naming", "proto", `naming convention. Use "proto" for passing names directly from the proto files`),
+	// 	FQSchemaNaming:  flags.Bool("fq_schema_naming", false, `schema naming convention. If "true", generates fully-qualified schema names by prefixing them with the proto message package name`),
+	// 	EnumType:        flags.String("enum_type", "string", `type for enum serialization. Use "string" for string-based serialization`),
+	// 	CircularDepth:   flags.Int("depth", 2, "depth of recursion for circular messages"),
+	// 	DefaultResponse: flags.Bool("default_response", true, `add default response. If "true", automatically adds a default response to operations which use the google.rpc.Status message. Useful if you use envoy or grpc-gateway to transcode as they use this type for their default error responses.`),
+	// 	OutputMode:      flags.String("output_mode", "merged", `output generation mode. By default, a single openapi.yaml is generated at the out folder. Use "source_relative' to generate a separate '[inputfile].openapi.yaml' next to each '[inputfile].proto'.`),
+	// }
+
+	var flags flag.FlagSet
+	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
+	useGenericStreams = flags.Bool("use_generic_streams_experimental", false, "set to true to use generic types for streaming client and server objects; this flag is EXPERIMENTAL and may be changed or removed in a future release")
+
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			NewValidateGeneratr(gen, f).Run()
+		}
+		return nil
+	})
+}
